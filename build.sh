f59a3205485ff34b7ef30df8d529c2d36aa0d63b
benjfranklin <benjaminfranklinhill@gmail.com>
2020-06-15 15:17:04 +0100

    edited build.sh

--- build.sh
+++ build.sh
@@ -13,8 +13,6 @@ tag="${4:-latest}"
 
 (cd "$source_path" && docker build -t "$image_name" .)
 
-#$(aws ecr get-login --no-include-email --region "$region")
-#$(aws ecr get-login-password --region $region --no-verify-ssl | docker login --username AWS --password-stdin $repository_url)
 docker tag "$image_name" "$repository_url":"$tag"
 docker push "$repository_url":"$tag"
 
